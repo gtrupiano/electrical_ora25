c9b16657a00209734744ef2bfa6781af08404c74
NerduMiner <nerdyminer18@gmail.com>
2026-05-22 01:00:02 +0000

    EStop: Update Code For latest changes
    The system now consists of the remote toggle sending packets to the receiver until power is cut to it, at which point the receiver activates EStop.

--- 2025RemoteEStop/xbee_recieve/xbee_recieve.ino
+++ 2025RemoteEStop/xbee_recieve/xbee_recieve.ino
@@ -1,14 +1,19 @@
 // Onboard Module
 #define RX 17
 #define TX 16
-#define ESTOP 5
+#define ESTOP 27 // HIGH: Not EStopped, LOW: EStopped
+
+// #define DEBOUNCE_LIMIT 8
+// unsigned int debounce_count = 0;
+// char filter_data = 'h';
 
 unsigned long watchdog = 0; // Current time since last ping
 unsigned long last_ping = 0; // Time of last ping
 bool estopped = false;
 
 void setup() {
-  Serial2.begin(9600, SERIAL_8N1, RX, TX);
+  //Serial.begin(9600);
+  Serial2.begin(115200, SERIAL_8N1, RX, TX);
   pinMode(ESTOP, OUTPUT);
   digitalWrite(ESTOP, LOW);
 }
@@ -22,23 +27,38 @@ void loop() {
     switch (data) {
       case 'h': // "Hey there"
         last_ping = millis();
-        break;
-      case 's': // "Stop"
-        last_ping = millis();
-        digitalWrite(ESTOP, HIGH);
-        estopped = true;
-        break;
-      case 'g': // "Go"
-        last_ping = millis();
-        digitalWrite(ESTOP, LOW);
-        estopped = false;
+        //Serial.println("h");
+        if (estopped)
+        {
+          digitalWrite(ESTOP, HIGH);
+          estopped = false;
+        }
         break;
     }
   }
+
+  // if (data == filter_data)
+  //   debounce_count += 1;
+  // else
+  //   debounce_count = 0;
+
+  // if (debounce_count >= DEBOUNCE_LIMIT)
+  // {
+  //   last_ping = millis();
+  //   if (estopped)
+  //   {
+  //     digitalWrite(ESTOP, HIGH);
+  //     estopped = false;
+  //   }
+  //   filter_data = data;
+  // }
+
   // Determine if we need to stop based on Watchdog Time
   // TODO: Determine best timeout time based on latency
-  if (watchdog >= 5000 && !estopped) { 
-    digitalWrite(ESTOP, HIGH);
+  if (watchdog > 175 && !estopped) { 
+    digitalWrite(ESTOP, LOW);
+    //Serial.print("Watchdog activated, delaytime: ");
+    //Serial.println(watchdog);
     estopped = true;
   }
 }
--- 2025RemoteEStop/xbee_send/xbee_send.ino
+++ 2025RemoteEStop/xbee_send/xbee_send.ino
@@ -1,31 +1,22 @@
 // Remote Module
-#define RX 17
-#define TX 16
-#define ESTOP 5
+#define RX 17 //on ESP32-WROOM
+#define TX 16 //on ESP32-WROOM
+
+//const uint8_t RX = 3;
+//const uint8_t TX = 1;
 
 unsigned long sent_delay = 0;
 unsigned long last_write = 0;
-bool estopped = false;
 
 void setup() {
-  Serial2.begin(9600, SERIAL_8N1, RX, TX);
-  pinMode(ESTOP, INPUT);
+  Serial2.begin(115200, SERIAL_8N1, RX, TX);
+  //Serial1.begin(9600);
 }
 
 void loop() {
   sent_delay = millis() - last_write;
-  if (sent_delay >= 1000) {
+  if (sent_delay >= 5) {
     last_write = millis();
     Serial2.write('h');
-  }
-  if (digitalRead(ESTOP) == HIGH && !estopped) {
-    last_write = millis();
-    Serial2.write('s');
-    estopped = true;
-  }
-  if (digitalRead(ESTOP) == LOW && estopped) {
-    last_write = millis();
-    Serial2.write('g');
-    estopped = false;
   }
 }
